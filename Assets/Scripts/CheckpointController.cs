f6a2dbced1a9f71b2c8737777935b08b889fe67c
Brice Yuuki Gibson <Kakipea888@gmail.com>
2023-07-16 00:05:41 -0500

    Player Respawn
    The player character now respawns after death to the beginning of the stage if no checkpoints are active or the active checkpoint.

--- Assets/Scripts/CheckpointController.cs
+++ Assets/Scripts/CheckpointController.cs
@@ -19,6 +19,8 @@ public class CheckpointController : MonoBehaviour
     void Start()
     {
         checkpoints = FindObjectsOfType<Checkpoint>();
+
+        spawnPoint = PlayerController.instance.transform.position;
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -51,7 +51,9 @@ public class PlayerHealthController : MonoBehaviour
             {
                 currentHealth = 0;
 
-                gameObject.SetActive(false);
+                //gameObject.SetActive(false);
+
+                LevelManager.instance.RespawnPlayer();
             }
             else
             {
