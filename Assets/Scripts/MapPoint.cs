739b50cceb27bff9381f575ea2dddc8d7ada2023
Brice Yuuki Gibson <Kakipea888@gmail.com>
2023-07-24 14:16:53 -0500

    Level Select Map
    The player is now able to select which levels to play through the level select map. The player is able to select the level by going to the level  (WASD) and selecting the map with the jump button (Space Bar). If the level is locked the player is unable to play the level.

---
+++ Assets/Scripts/MapPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPoint : MonoBehaviour
+{
+    public MapPoint up, right, down, left;
+    public bool isLevel, isLocked;
+    public string levelToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
