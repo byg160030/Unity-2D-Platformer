c9b5b3de46c05ee09ce7ade8ce493c96517ec4ad
Brice Yuuki Gibson <Kakipea888@gmail.com>
2023-07-30 23:31:39 -0500

    Eagle Enemy Added Hint For Missile System
    The enemy type eagle is added to the game though it still does not hurt the player character it does chase the player character when the player character is within the attack range.

---
+++ Assets/Scripts/FlyingEnemyController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyController : MonoBehaviour
+{
+    public Transform[] points;
+    public float moveSpeed;
+    private int currentPoint;
+
+    public SpriteRenderer theSR;
+
+    public float distanceToAttackPlayer, chaseSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < points.Length; i++)
+        {
+            points[i].parent = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, PlayerController.instance.transform.position) > distanceToAttackPlayer)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, points[currentPoint].position, moveSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, points[currentPoint].position) < .05f)
+            {
+                currentPoint++;
+
+                if (currentPoint >= points.Length)
+                {
+                    currentPoint = 0;
+                }
+            }
+
+            if (transform.position.x < points[currentPoint].position.x)
+            {
+                theSR.flipX = true;
+            }
+            else if (transform.position.x > points[currentPoint].position.x)
+            {
+                theSR.flipX = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, PlayerController.instance.transform.position, chaseSpeed * Time.deltaTime);
+        }
+    }
+}
